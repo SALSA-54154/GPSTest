b092462381edff8c4699394830e173eb56d721aa
Ian Lansdowne <idlansdowne@gmail.com>
2021-09-17 07:25:34 -0500

    adding current x and y values

--- src/main.cpp
+++ src/main.cpp
@@ -152,8 +152,8 @@ void goTo(double ix, double iy, double iyaw)
 		//y /= 2.0;
 
 		//PID initial concept
-		double x_power = PID(x, 0, proportional_tune, derivative_tune);
-		double y_power = PID(y, 0, proportional_tune, derivative_tune);
+		double x_power = PID(x, gpsData.x, proportional_tune, derivative_tune);
+		double y_power = PID(y, gpsData.y, proportional_tune, derivative_tune);
 		double yaw_power = PID(iyaw, yaw, turn_proportional_tune, turn_derivative_tune);
 
 		// Scale down the yaw value (and negate it) so at 180° from the target, it turns at 100% power
